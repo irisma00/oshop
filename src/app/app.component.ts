987d8bdc9bd9644a5b609ce18d9d554aee3ec941
Iris Ma <huaiyao@iastate.edu>
2022-09-06 15:29:22 -0700

    Initial push

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'oshop';
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAan0j5STb_d_xKzWowatMCEnUw_6riqUA",
+    authDomain: "oshop-e36a1.firebaseapp.com",
+    projectId: "oshop-e36a1",
+    storageBucket: "oshop-e36a1.appspot.com",
+    messagingSenderId: "494842514488",
+    appId: "1:494842514488:web:29b65fcd5516c4005daacd",
+    measurementId: "G-0FVHEWDR44"
+  }
+};
